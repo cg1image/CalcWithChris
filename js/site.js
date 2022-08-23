c95564773c5ebea6d92c21c0eb0fb2958a82a02f
CGovers <54491781+cg1image@users.noreply.github.com>
2022-08-22 21:55:03 -0700

    create a calculator
    in progress.....

--- js/site.js
+++ js/site.js
@@ -1,7 +1,42 @@
 
 function calcOne () {
     document.getElementById("calcField").textContent = 1;
+    let one = document.getElementById("calcField").textContent;
+    one = parseInt(one);
+    return one;
 }
+
+
+function calcTwo () {
+    document.getElementById("calcField").textContent = 2;
+    let two = document.getElementById("calcField").textContent;
+    two = parseInt(two);
+    return two;
+}
+
+
+function calcPlus () {
+    let x = document.getElementById("calcField").textContent;
+    document.getElementById("calcField").textContent = "";
+    calcEqual(x);
+    
+
+}
+
+
+
+
+function calcEqual () {
+  
+}
+
+function calcErase () {
+    document.getElementById("calcField").textContent = "";
+}
+
+
+
+
 
 
 /* Order of opertions:
@@ -13,11 +48,9 @@ function calcOne () {
  - Press =, the math operation of 1 + 2 should be resolved, then
  displayed to the display field.
 
-
-*/
-
 document.getElementById("calcField").textContent = 5; // puts a value in field
 let x = document.getElementById("calcField").textContent; // assigns value to variable
 x = parseInt(x);
 console.log(x);
 console.log(typeof(x));
+*/
